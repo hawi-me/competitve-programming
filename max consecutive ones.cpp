21040e2ea75dbde481fe19df75a638f222b663ab
hawi-me <114941392+hawi-me@users.noreply.github.com>
2023-02-10 21:31:01 -0800

    Create max consecutive ones.cpp

---
+++ max consecutive ones.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        vector<int> zeroes ; 
+        zeroes.push_back(-1) ;
+        for(int i = 0 ; i< nums.size() ; ++i) {
+            if (nums[i] == 0) zeroes.push_back(i) ;
+            
+        }
+        zeroes.push_back(nums.size()) ;
+        if (k >= zeroes.size() - 2) 
+        return nums.size() ;
+        int start = 0  ;
+        int end = k + 1 ;
+        int result= 0 ;
+        while (end < zeroes.size()) {
+            result = max( result, zeroes[end] - zeroes[start] - 1) ;
+            start++ ;
+            end++ ;
+        }
+        return result;
+    }
+};
