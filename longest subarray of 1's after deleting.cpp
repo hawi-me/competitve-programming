eafdfac2de760d699cc682905a103c9e2bffbdfc
hawi-me <114941392+hawi-me@users.noreply.github.com>
2023-02-11 08:52:17 -0800

    Create longest subarray of 1's after deleting.cpp

---
+++ longest subarray of 1's after deleting.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& A) {
+        int i = 0, j, k = 1;
+        for (j = 0; j < A.size(); ++j) {
+            if (A[j] == 0)
+             k--;
+            if (k < 0 && A[i++] == 0)
+             k++;
+        }
+        int result= j - i - 1;
+        return result;
+    }
+
+};
