7508faf8b37cdb621d55364c801f9a14129039db
hawi-me <114941392+hawi-me@users.noreply.github.com>
2023-02-11 23:32:30 -0800

    Create Array With Elements Not Equal to Average of Neighbors.cpp

---
+++ Array With Elements Not Equal to Average of Neighbors.cpp
@@ -0,0 +1,12 @@
+
+class Solution {
+public:
+
+    vector<int> rearrangeArray(vector<int>& nums) {
+          for(int i=1;i+1<nums.size();i++){
+                    int x=nums[i-1],y=nums[i],z=nums[i+1];
+                    if( x<y&& y<z|| x>y &&y>z  ) swap(nums[i],nums[i+1]);
+          }
+        return nums;
+    }
+    };
