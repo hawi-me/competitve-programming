a3059243ba30640046f80e281c9e9a6aafbc3c4d
hawi-me <114941392+hawi-me@users.noreply.github.com>
2023-11-29 19:20:43 +0300

    Added solution for Toeplitz Matrix

---
+++ leet-code-solutions/toeplitz-matrix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        for i in range(0, len(matrix) - 1):
+            for j in range(0, len(matrix[0]) - 1):
+                if matrix[i][j] != matrix[i + 1][j + 1]:
+                    return False
+        return True
+
+        
+        
